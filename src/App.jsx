522119de5d25d891514f8a7c3fa4c55373035d8e
Ahmed Lotfy <59890491+ahmed-lotfy-dev@users.noreply.github.com>
2023-03-12 17:17:33 +0200

    Remove code i was testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 
-const signWithGoogleHandler = () => {
-  console.log("signin");
-};
-
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1 className="text-3xl bg-blue-400 font-bold underline">
-        Welcome To The Main Project
+      <h1 className="text-3xl bg-blue-300 font-bold underline">
+        Welcome To The Main Project of Team-14
       </h1>
-      {/* <button onClick={signWithGoogleHandler}>Signin With Google</button> */}
-      <h1>Sign in</h1>
-      <a className="button google" href="/login/federated/google">
-        Sign in with Google
-      </a>
+
     </div>
   );
 }
