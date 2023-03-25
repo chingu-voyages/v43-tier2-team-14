82bce75e311e1daede228108ed425907ad32cad5
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-25 23:37:06 +0200

    google auth working fully functionall and the req.user working on the frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,9 +4,12 @@ const Home = () => {
   const [user, setUser] = useState("");
 
   const fetchData = async () => {
-    const res = await fetch("http://localhost:4000/api/user", {
-      credentials: "include",
-    });
+    const res = await fetch(
+      "https://v43-tier2-team14-backend.onrender.com/api/user",
+      {
+        credentials: "include",
+      }
+    );
     console.log(res);
     const userData = await res.json();
     console.log(userData);
@@ -23,7 +26,9 @@ const Home = () => {
       <h1 className="text-3xl bg-blue-300 font-bold underline">
         Welcome To The Main Project of Team-14
       </h1>
-      <a href="http://localhost:4000/auth/google">Login</a>
+      <a href="https://v43-tier2-team14-backend.onrender.com/auth/google">
+        Login
+      </a>
     </div>
   );
 };
