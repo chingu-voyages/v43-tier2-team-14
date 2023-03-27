f2042d9ee653f7039ef2e6733eb34c1984eff0f3
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-27 04:53:24 +0200

    add zustand user store and create search component

--- frontend/src/features/userStore.jsx
+++ frontend/src/features/userStore.jsx
@@ -1,12 +1,8 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
-import { persist } from "zustand/middleware";
-
 const store = (set) => ({
   user: [],
   getUserData: async () => {
-    const url = `${import.meta.env.VITE_BACKEND_URL}/api/user`;
-    console.log(url);
+    const url = `http://localhost:4000/api/user`;
+
     try {
       const res = await fetch(url, {
         credentials: "include",
@@ -25,7 +21,7 @@ const store = (set) => ({
   //   set((state) => ({
   //     wishList: [...state.wishList, item],
   //   })),
-});
+}); // <-- add closing brace here
 
 export const userStore = create(
   persist(devtools(store), { name: "userStore" })
