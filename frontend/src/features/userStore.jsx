e58c3a3b7ddd6e7063f7f743fd4a689af0c7d0bd
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-28 05:06:57 +0200

    login working logout working userstore in the state of zustand search component

--- frontend/src/features/userStore.jsx
+++ frontend/src/features/userStore.jsx
@@ -7,11 +7,9 @@ export const userStore = create((set) => ({
     const url = `${import.meta.env.VITE_BACKEND_URL}`;
     const res = await fetch(`${url}/api/user`, {
       credentials: "include",
-    });
-    const resData = await res.text();
-    const userData = resData ? JSON.parse(resData) : null;
-    console.log("User data:", userData);
-    set({ user: userData });
+    })
+      .then((response) => response.json())
+      .then((data) => set({ user: data }));
   },
   logout: async () => set({ user: null }),
 }));
