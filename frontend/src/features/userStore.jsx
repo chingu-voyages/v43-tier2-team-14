e577796eab032af41e769efd94c12012375d6045
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-28 06:10:45 +0200

    trying

--- frontend/src/features/userStore.jsx
+++ frontend/src/features/userStore.jsx
@@ -5,11 +5,11 @@ export const userStore = create((set) => ({
   user: null,
   fetchUserData: async () => {
     const url = `${import.meta.env.VITE_BACKEND_URL}`;
-    const res = await fetch(`${url}/api/user`, {
+    const response = await fetch(`${url}/api/user`, {
       credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((data) => set({ user: data }));
+    });
+    const data = await response.json();
+    set({ user: data });
   },
 
   logout: async () => set({ user: null }),
