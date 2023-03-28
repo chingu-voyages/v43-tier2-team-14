7156b49035fc3ece0979dfcb7e1df2da7fb6b039
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-28 23:54:32 +0200

    trying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dotenv from "dotenv"
 dotenv.config()
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({
     include: "**/*.jsx",
   })],
+  server: {
+    cors: true,
+    headers: true,
+  },
 })
