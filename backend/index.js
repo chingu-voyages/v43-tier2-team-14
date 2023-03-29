4d5f86b477db2ce098ee422f5e7a7749fb1b5636
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-29 20:30:42 +0200

    trying

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.use('/auth', authRoutes)
 
 app.get('/api/user', (req, res, next) => {
   // res.json(req.user);
-  res.send(req.user);
+  res.json(req.user);
   console.log(req.user)
   console.log(req.session.passport)
 });
