6acd13c77c3b7d1ba1458a179165f759cc78d525
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-29 20:18:34 +0200

    trying

--- frontend/src/features/userStore.jsx
+++ frontend/src/features/userStore.jsx
@@ -1,23 +1,9 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import axios from "axios";
 
 export const userStore = create((set) => ({
   user: null,
-  fetchUserData: async () => {
-    try {
-      const userData = await axios.get(
-        `${import.meta.env.VITE_BACKEND_URL}/api/user`,
-        {
-          withCredentials: true,
-          credentials: "include",
-        }
-      );
-      console.log(userData);
-      set({ user: userData });
-    } catch (error) {
-      console.log(error);
-    }
-  },
+  updateUser: (user) => set(() => ({ user: user })),
+
   logout: async () => set({ user: null }),
 }));
--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -4,18 +4,36 @@ import Footer from "../components/Footer";
 import { bookStore } from "../features/bookStore";
 import Sidebar from "../components/Sidebar";
 import { userStore } from "../features/userStore";
+import axios from "axios";
+
+const fetchUserData = async () => {
+  try {
+    const userData = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/api/user`,
+      {
+        withCredentials: true,
+        credentials: "include",
+      }
+    );
+    console.log(userData);
+    userStore.getState.set({ user: userData });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const Root = () => {
   const getBooks = bookStore((state) => state.getBooks);
   const bookList = bookStore((state) => state.bookList);
   const user = userStore((state) => state.user);
-  const fetchUser = userStore((state) => state.fetchUserData);
+  const updateUser = userStore((state) => state.updateUser);
 
   useEffect(() => {
     getBooks();
     console.log(bookList);
-    fetchUser();
+    fetchUserData();
     console.log(user);
+    updateUser(user);
   }, []);
 
   return (
