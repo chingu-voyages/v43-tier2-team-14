6adc407cabe511f1d73f2ccc06a345d1250379b4
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-29 20:23:01 +0200

    trying

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import { bookStore } from "../features/bookStore";
@@ -6,34 +6,35 @@ import Sidebar from "../components/Sidebar";
 import { userStore } from "../features/userStore";
 import axios from "axios";
 
-const fetchUserData = async () => {
-  try {
-    const userData = await axios.get(
-      `${import.meta.env.VITE_BACKEND_URL}/api/user`,
-      {
-        withCredentials: true,
-        credentials: "include",
-      }
-    );
-    console.log(userData);
-    userStore.getState.set({ user: userData });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const Root = () => {
+  const [userData, setUserData] = useState({});
   const getBooks = bookStore((state) => state.getBooks);
   const bookList = bookStore((state) => state.bookList);
   const user = userStore((state) => state.user);
   const updateUser = userStore((state) => state.updateUser);
 
+  const fetchUserData = async () => {
+    try {
+      const userInfo = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/api/user`,
+        {
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
+      console.log(userInfo);
+      setUserData(userInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getBooks();
     console.log(bookList);
     fetchUserData();
-    console.log(user);
-    updateUser(user);
+    console.log(userData);
+    updateUser(userData);
   }, []);
 
   return (
