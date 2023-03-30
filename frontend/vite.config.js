1b3f3a100235b00a6e4f6a628d392bc1a6cce957
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-30 22:44:23 +0200

    trying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dotenv from "dotenv"
 dotenv.config()
-import dns from 'dns'
-
-dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({
