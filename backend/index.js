54dc1f3713a63b944aa6298f0af5461fd320228e
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-31 00:14:41 +0200

    fixing fetching books

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use(session({
 }));
 
 app.use(cors({
-  origin: APP_HOME,
+  origin: [APP_HOME,'http://localhost:5173'],
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -6,14 +6,14 @@ import Sidebar from "../components/Sidebar";
 import { userStore } from "../features/userStore";
 
 const Root = () => {
-  // const getBooks = bookStore((state) => state.getBooks);
-  // const bookList = bookStore((state) => state.bookList);
+  const getBooks = bookStore((state) => state.getBooks);
+  const bookList = bookStore((state) => state.bookList);
   const user = userStore((state) => state.user);
   const fetchUserData = userStore((state) => state.fetchUserData);
 
   useEffect(() => {
-    // getBooks();
-    // console.log(bookList);
+    getBooks();
+    console.log(bookList);
     console.log(user);
     const isAuthenticated = localStorage.getItem("isAuthenticated");
     console.log(isAuthenticated);
