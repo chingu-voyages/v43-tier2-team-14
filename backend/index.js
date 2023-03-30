030a420348287adfdc702e2487246b7c4164b9bc
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-31 00:22:39 +0200

    fixing fetching books

--- backend/index.js
+++ backend/index.js
@@ -45,10 +45,10 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${APP_HOME} `);
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Headers', 'Authorization');
+  res.setHeader('Access-Control-Allow-Origin', `${APP_HOME} `);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization');
   next();
 });
 
