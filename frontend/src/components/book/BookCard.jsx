2ce61dfc604fb544e6dfcb0566a5b67ef7ccecc3
Muhammad Rabi <vonrabi07@yahoo.com>
2023-03-31 16:43:03 +0200

    Style-books-container

--- frontend/src/components/book/BookCard.jsx
+++ frontend/src/components/book/BookCard.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
-import { BsBookmark } from "react-icons/bs";
+import { BsBookmarkPlus, BsBookmarkDashFill } from "react-icons/bs";
 import { bookStore } from "../../features/bookStore";
 import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
 
 const BookCard = ({
   id,
@@ -25,7 +26,6 @@ const BookCard = ({
     description,
     authors,
   };
-  console.log(item);
 
   const notify = () =>
     toast.success(`${title} Added to your wish list`, {
@@ -35,24 +35,28 @@ const BookCard = ({
   const addItemToWishlist = (item) => {
     addToWishlist(item);
     notify();
+    setIsAdded(!isAdded);
   };
 
+  const [isAdded, setIsAdded] = useState(false);
   return (
-    <article className="border border-zinc-400 rounded-sm px-2 py-6 flex flex-col items-center justify-between">
-      <figure className="flex cursor-pointer">
+    <article className="max-w-xs border bg-zinc-100 border-slate-300 shadow-md rounded-sm px-2 py-6 flex flex-col items-center justify-between">
+      <figure className="flex cursor-pointer mb-2">
         <Link to={`${id}`}>
           <img src={imageLinks.thumbnail} alt="" />
         </Link>
       </figure>
-      <div className="px-4 text-center">
-        <h2 className="mb-4">{title}</h2>
-        <p>{categories}</p>
+      <div className="px-4 text-center flex flex-col items-center space-y-2">
+        <h2 className="font-semibold">{title.substr(0, 25)}</h2>
+        <p className="px-2 py-0.5 text-xs bg-red-500 text-white rounded-md w-fit">
+          {categories}
+        </p>
         <span>{pageCount} pages</span>
         <div
-          className="p-3 flex justify-center w-12 mx-auto cursor-pointer duration-300 text-text-btn hover:text-text-main"
+          className="p-3 text-xl flex justify-center w-12 mx-auto cursor-pointer duration-300 text-text-btn hover:text-text-main"
           onClick={() => addItemToWishlist(item)}
         >
-          <BsBookmark />
+          {!isAdded ? <BsBookmarkPlus /> : <BsBookmarkDashFill />}
         </div>
       </div>
       <Toaster />
--- frontend/src/components/wishList/WishListItem.jsx
+++ frontend/src/components/wishList/WishListItem.jsx
@@ -1,33 +1,44 @@
-import { CiBookmarkRemove } from "react-icons/ci"
-import { bookStore } from "../../features/bookStore"
+import { BsBookmarkDashFill } from "react-icons/bs";
+import { bookStore } from "../../features/bookStore";
+import toast, { Toaster } from "react-hot-toast";
 
 const WishListItem = ({
   title,
   id,
   pageCount,
   authors,
+  categories,
   description,
   imageLinks,
 }) => {
-  const removeFromWishlist = bookStore((state) => state.removeFromWishlist)
+  const removeFromWishlist = bookStore((state) => state.removeFromWishlist);
 
   const removeItemFromWishlist = (id) => {
-    removeFromWishlist(id)
-  }
+    removeFromWishlist(id);
+    toast.error(`${title} has been removed from your wish list`, {
+      position: "top-right",
+    });
+  };
 
   return (
-    <article className="w-full md:w-3/4 mx-auto shadow-md bg-zinc-100 flex justify-between items-center mb-6 p-4 border-2 rounded-md hover:shadow-sm ">
+    <article className="w-full md:w-3/6 mx-auto shadow-md bg-zinc-100 flex justify-around items-center my-6 p-4 space-x-4 border-2 rounded-md hover:shadow-sm">
       <img src={imageLinks.thumbnail} alt="" />
-      <h2>{title}</h2>
-      <span>{authors}</span>
-      <p>{pageCount} pages</p>
+      <div className="flex flex-col p-4 flex-1 space-y-4">
+        <h2 className="text-xl tracking-wider font-bold">{title}</h2>
+        <span className="text-red-600 font-semibold">{authors}</span>
+        <p className="px-2 py-0.5 text-xs bg-red-500 text-white rounded-md w-fit">
+          {categories}
+        </p>
+        <p>{pageCount} pages</p>
+      </div>
       <div
-        className="cursor-pointer text-3xl duration-300 hover:text-white hover:bg-bg-btn"
+        className="cursor-pointer text-2xl p-4 duration-300 self-end hover:text-slate-600"
         onClick={() => removeItemFromWishlist(id)}
       >
-        <CiBookmarkRemove />
+        <BsBookmarkDashFill />
       </div>
+      <Toaster />
     </article>
-  )
-}
-export default WishListItem
+  );
+};
+export default WishListItem;
