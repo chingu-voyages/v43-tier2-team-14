8cacc885ba516708cd412d401b00a6f69ccb9350
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-04-11 21:51:31 +0200

    fixing keys conflict for react component array map

--- frontend/src/components/featured/FeaturedList.jsx
+++ frontend/src/components/featured/FeaturedList.jsx
@@ -8,17 +8,14 @@ const FeaturedList = ({ list }) => {
     <div className="flex gap-6">
       {books.map((book) => {
         return (
-          <div
-            key={book.primary_isbn10}
-            className="flex flex-col w-full  items-start "
-          >
+          <div key={book.title} className="flex flex-col w-full  items-start ">
             <h4 className="w-52 px-6 py-3 bg-slate-400 rounded-lg font-light text-sm text-center">
               {book.title}
             </h4>
             <img
               className="w-52 pt-6"
               src={book.book_image}
-              alt={`${book.title} image`}
+              alt={`${book.title} Cover Image`}
             />
           </div>
         );
