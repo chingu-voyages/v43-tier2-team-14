98dbb81fed49429cac1e4ee868537932081f9f6c
Muhammad Rabi <vonrabi07@yahoo.com>
2023-04-15 23:54:14 +0200

    build cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,10 +1,10 @@
-import HomeBtn from "../components/UI/HomeBtn";
+import CartWrapper from "../components/cart/CartWrapper";
+import EmptyCart from "../components/cart/EmptyCart";
 
 const Cart = () => {
   return (
-    <section className="col-start-3 col-span-full">
-      <div className="text-5xl font-bold ">Cart page</div>
-      <HomeBtn />
+    <section className="flex flex-col items-center justify-start col-start-3 mt-24 col-span-full">
+      <CartWrapper />
     </section>
   );
 };
