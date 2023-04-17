acbdc55dd3e7cb3da4650d4f61e4650e260f7bfc
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-04-17 23:36:44 +0200

    creating cart schema and cartitem schema

--- backend/models/cartItemModel.js
+++ backend/models/cartItemModel.js
@@ -4,7 +4,8 @@ const CartItemShema = Schema({
   name: String,
   price: String,
   image: String,
-  author: String
+  author: String,
+  quantity: String
 }, { timestamps: true })
 
 const CartItem = model('CartItem', CartItemShema)
--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -1,7 +1,6 @@
 import { model, Schema } from "mongoose";
 
 const CartSchema = Schema({
-  cartId: String,
   cartItem: [{ type: Schema.Types.ObjectId, ref: 'Cart' }],
   userId: { type: Schema.Types.ObjectId, ref: 'User' },
 }, { timestamps: true })
